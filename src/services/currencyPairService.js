afe6d73a2e032739bfb56c5d251c22d4772634ae
Zack <aminuzack7@gmail.com>
2019-07-06 16:42:36 +0100

    refactor

--- src/services/currencyPairService.js
+++ src/services/currencyPairService.js
@@ -1,10 +1,10 @@
 import React, {useState ,useEffect} from 'react';
 import axios from 'axios';
 import CurrencyPairDropDdown from '../components/currencyPairDropdown';
+import {toast} from 'react-toastify';
 
 export function GetCurrencyPairs() {
     const [currencyPairs, setCurrencyPairs] = useState([]);
-
     
     useEffect(() => {
         const fetchCurrencyPair = async () => {
@@ -12,7 +12,7 @@ export function GetCurrencyPairs() {
                 const {data} = await axios.get('https://www.bitstamp.net/api/v2/trading-pairs-info/');
                 setCurrencyPairs(data);
             } catch (error) {
-                console.log(error);
+                toast.error(`${error}, unable to fetch currency pairs. Check your internet connection`);
             }
         };
         fetchCurrencyPair();
