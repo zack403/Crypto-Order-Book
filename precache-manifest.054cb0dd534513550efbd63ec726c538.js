de4d2444cd2a9b0f4077f3c01b54a0b2423155ea
Zack <aminuzack7@gmail.com>
2019-07-11 16:59:55 +0100

    Updates

---
+++ precache-manifest.054cb0dd534513550efbd63ec726c538.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b489835d28944d43c0f59cdf6f47d9",
+    "url": "/Crypto-Order-Book/index.html"
+  },
+  {
+    "revision": "1648bb6793914e0c5515",
+    "url": "/Crypto-Order-Book/static/css/2.0c3234d5.chunk.css"
+  },
+  {
+    "revision": "80b2347bc1df75cc510c",
+    "url": "/Crypto-Order-Book/static/css/main.d69faa60.chunk.css"
+  },
+  {
+    "revision": "1648bb6793914e0c5515",
+    "url": "/Crypto-Order-Book/static/js/2.e3dccbf8.chunk.js"
+  },
+  {
+    "revision": "80b2347bc1df75cc510c",
+    "url": "/Crypto-Order-Book/static/js/main.7d1b414f.chunk.js"
+  },
+  {
+    "revision": "b36b8383a76f95859ea6",
+    "url": "/Crypto-Order-Book/static/js/runtime~main.4686f22c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Crypto-Order-Book/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Crypto-Order-Book/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Crypto-Order-Book/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Crypto-Order-Book/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Crypto-Order-Book/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
